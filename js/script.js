fd45b92e3f0c478a6d29fbb7fa35ccf16f5efb44
vitodepi16 <118888625+vitodepi16@users.noreply.github.com>
2023-03-09 15:44:30 +0100

    test input js

--- js/script.js
+++ js/script.js
@@ -10,47 +10,83 @@ Il programma dovrà chiedere all'utente il numero di chilometri che vuole percor
 */
 
 
-const ageUser = prompt('Inserisci la tua Età');
-const travelKm = prompt('Per quanti km intendi viaggiare?');
-const priceKm = 0.21;
-let totalPrice = (travelKm * priceKm).toFixed(2);
-const discountYoung = ((totalPrice) * 0.20).toFixed(2);
-const discountOld =  ((totalPrice) * 0.40).toFixed(2);
-let priceDiscountYoung = (totalPrice - discountYoung ).toFixed(2);
-let priceDiscountold = (totalPrice - discountOld).toFixed(2);
+// const ageUser = prompt('Inserisci la tua Età');
+// const travelKm = prompt('Per quanti km intendi viaggiare?');
+// const priceKm = 0.21;
+// let totalPrice = (travelKm * priceKm).toFixed(2);
+// const discountYoung = ((totalPrice) * 0.20).toFixed(2);
+// const discountOld =  ((totalPrice) * 0.40).toFixed(2);
+// let priceDiscountYoung = (totalPrice - discountYoung ).toFixed(2);
+// let priceDiscountold = (totalPrice - discountOld).toFixed(2);
 
-document.getElementById('priceticket').innerHTML = `
+const ageBox = document.querySelector('input=[name="age"]');
+const kmBox = document.querySelector('input=[name="km"]');
+
+const button = document.querySelector('button');
+console.log(button);
+
+
+button.addEventListener('click', function () {
+    let ageUser = ageBox.value;
+    let travelKm = kmBox.value;
+    const priceKm = 0.21;
+    let totalPrice = (travelKm * priceKm).toFixed(2);
+    // const discountYoung = ((totalPrice) * 0.20).toFixed(2);
+    // const discountOld =  ((totalPrice) * 0.40).toFixed(2);
+    // let priceDiscountYoung = (totalPrice - discountYoung ).toFixed(2);
+    // let priceDiscountold = (totalPrice - discountOld).toFixed(2);
+    totalPrice.innerHTML =  `
+    <h1>Il Prezzo del tuo biglietto é:  ${totalPrice}&euro; </h1>
+    `;
+    
+
+
+
+    console.log(ageUser);
+    console.log(travelKm);
+})
+
+document.getElementById('priceticket').innerHTML=`
 <h1>Il Prezzo del tuo biglietto é:  ${totalPrice}&euro; </h1>
 `;
 
-if(isNaN(ageUser), isNaN(travelKm)){
-    document.getElementById('priceticket').innerHTML = `
-    <h1>Inserisci correttamente i tuoi dati</h1>
-    `;
-  
-}
 
-    else  if (ageUser < 18){
-    totalPrice = discountYoung, 
-    document.getElementById('discountTicket').innerHTML = `
-    <h2>Lo sconto del biglietto é: ${totalPrice}&euro; </h2>
-    `;
-    document.getElementById('priceDiscountYoung').innerHTML = ` 
-    <h1>Totale da pagare:${priceDiscountYoung}&euro;</h1>
-    `;
-}
-    else if(ageUser > 64){
-        totalPrice = discountOld,
-        document.getElementById('discountTicket').innerHTML = `
-        <h2>Lo sconto del biglietto é di: ${totalPrice}&euro; </h2>
-        `;
-        document.getElementById('priceDiscountOld').innerHTML = ` 
-        <h1>Totale da pagare:${priceDiscountold}&euro;</h1>
-        `;
-    } 
-    else {
-        document.getElementById('priceticket').innerHTML = `
-<h1>Il Prezzo del tuo biglietto:   ${totalPrice}&euro; </h1>
-`;
-    }
+
+
+
+
+
+
+
+
+// if(isNaN(ageUser), isNaN(travelKm)){
+//     document.getElementById('priceticket').innerHTML = `
+//     <h1>Inserisci correttamente i tuoi dati</h1>
+//     `;
+  
+// }
+
+//     else  if (ageUser < 18){
+//     totalPrice = discountYoung, 
+//     document.getElementById('discountTicket').innerHTML = `
+//     <h2>Lo sconto del biglietto é: ${totalPrice}&euro; </h2>
+//     `;
+//     document.getElementById('priceDiscountYoung').innerHTML = ` 
+//     <h1>Totale da pagare:${priceDiscountYoung}&euro;</h1>
+//     `;
+// }
+//     else if(ageUser > 64){
+//         totalPrice = discountOld,
+//         document.getElementById('discountTicket').innerHTML = `
+//         <h2>Lo sconto del biglietto é di: ${totalPrice}&euro; </h2>
+//         `;
+//         document.getElementById('priceDiscountOld').innerHTML = ` 
+//         <h1>Totale da pagare:${priceDiscountold}&euro;</h1>
+//         `;
+//     } 
+//     else {
+//         document.getElementById('priceticket').innerHTML = `
+// <h1>Il Prezzo del tuo biglietto:   ${totalPrice}&euro; </h1>
+// `;
+//     }
     
